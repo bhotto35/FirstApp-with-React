912fdd7aef2c8c073ff143cf304cbbd3d631cbaa
bhotto35 <71808978+bhotto35@users.noreply.github.com>
2021-04-02 19:45:59 +0530

    Assignment 2 part 1
    Adding default  Drawer Navigator and Material Tab screen, additional experimentation with Scroll View, Image Background, Image et cetera

--- App.js
+++ App.js
@@ -1,124 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-/*import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-                This is just a test line of text
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;*/
 //import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, Text, View, Button, Settings, Easing, Image} from 'react-native';
+import {StyleSheet,Dimensions,TouchableHighlight, Text, View, ScrollView, Button, Image, ImageBackground, Easing} from 'react-native';
 import {NavigationContainer, useIsFocused,getFocusedRouteNameFromRoute} from '@react-navigation/native';
 import {createStackNavigator,TransitionPresets,CardStyleInterpolators} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 //import Icon from 'react-native-ionicons';
 //import find from './icons/find.png';
@@ -126,8 +12,10 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 //import {Ionicons} from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();//createBottomTabNavigator();
 const HomeStack = createStackNavigator();
+//const DetailsStack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 //--FIRST FUNCTION THAT DOESN'T UPDATE TITLE-----//
 const HomeScreen = ({navigation}) =>{
@@ -141,10 +29,13 @@ const HomeScreen = ({navigation}) =>{
   return (<View style={{
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
-  }}>
-    <Text style={{color: 'green', fontSize: 24}}>Home Screen</Text>
-  <Button title="Go to Details screen" onPress={()=>navigation.navigate('Details')}/>
+    alignItems: "center",
+    backgroundColor: 'rgb(52,50,70)'
+  }} >
+    <Text style={{color: 'lawngreen', fontFamily: "Arial",fontSize: 24}}>Home Screen {"\n"}</Text>
+  <Button title="Go to Details screen" 
+  color = 'orange'
+  onPress={()=>navigation.navigate('Details')}/>
   
   </View>);
 }
@@ -161,15 +52,67 @@ const HomeScreen = ({navigation}) =>{
   </View>
 )*/
 
+const HomeStackScreen = ({navigation}) =>{
+  return(
+    <Stack.Navigator name = "Home" 
+      screenOptions = {{
+        headerStyle : { backgroundColor:'darkblue'},
+        headerTintColor : 'white',
+        headerTitleStyle: {fontFamily:'Baskerville Old Face',
+          fontWeight:'bold',
+          alignSelf:'flex-start',
+          }
+      }}
+      headerMode='float'
+      animation = 'fade' >
+        
+        <Stack.Screen name = "Home" options={({route})=>({
+          title:'Home',
+          headerLeft : ()=>(
+            <Icon.Button name = "list" backgroundColor="blue" color="white"
+            onPress = {()=>{navigation.openDrawer()}} />
+          ),
+        })} 
+        component = {HomeScreen}/>
+      </Stack.Navigator>
+  );
+}
+
+const DetailsStackScreen = ({navigation}) =>{
+  return(
+    <Stack.Navigator name = "Details" 
+      screenOptions = {{
+        headerStyle : { shadowColor : 'violet', shadowOffset:3,backgroundColor:'darkgreen'},
+        headerTintColor : 'white',
+        headerTitleStyle: {fontFamily:'Impact',fontWeight:'bold'}
+      }}
+      headerMode='float'
+      animation = 'fade' 
+      >
+        
+        <Stack.Screen name = "Details" options={({route})=>({
+          title:'More on This',
+          headerLeft : ()=>(
+            <Icon.Button name = "list" backgroundColor="green" color="white"
+            onPress = {()=>{navigation.openDrawer()}} />
+          )
+        })} 
+        component = {DetailsScreen}/>
+      </Stack.Navigator>
+  );
+}
+
 const SettingsScreen = ({navigation}) =>{
   const isFocused = useIsFocused();
 
   return (<View style={{
     flex: 1,
     justifyContent: "center",
+    backgroundColor:'purple',
     alignItems: "center"
   }}><Text style={{color: isFocused? 'grey': 'green', fontSize: 24}}>Settings</Text>
-  <Button title="Go to Home screen" onPress={()=>navigation.goBack()}/>
+  <Text style={{color:'white'}}>{'\n'} Yet to be thought of {'\n'} </Text>
+  <Button title="Go Back" onPress={()=>navigation.goBack()}/>
   </View>
 );}
 
@@ -179,7 +122,15 @@ const DetailsScreen = (props)=>
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
-  }}><Text style={{color: 'darkblue'}}>Details Screen</Text>
+  }}><Text style={{color: 'darkblue', fontSize: 24}}>
+    Details
+    {"\n"}
+  </Text>
+  <Text style = {{flex:0, flexDirection:'row'}}>
+    {'\u2022'} This ain't much 
+    {"\n"}
+    {'\u2022'} But it is honest work
+  </Text>
   </View>
 )
 
@@ -198,14 +149,124 @@ const HomeStackNavigator = ({navigation,route})=>{
   );
 };
 
-const FeedScreen = props=>(
+const FeedScreen = ({navigation,route})=>{
+  return(
   <View style={{
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }}><Text style={{color: 'darkblue'}}>Feed Screen</Text>
+    justifyContent: 'center', 
+    alignItems: 'center'
+  }}>
+    <Text style={{width:Dimensions.get('window').width,
+        flex:1,
+        fontSize:20,
+        textAlign:'center',
+        fontWeight:'bold',
+        color:'white',
+        paddingTop:10,
+        textShadowOffset:{width:10,height:10},
+        elevation:10,
+        backgroundColor:'rgba(50,50,50,1)'}}>
+          Feed
+    </Text>
+    <ImageBackground source={require('./bground_img/hehelol.png')} style={{flex: 14, width:450,resizeMode: "cover"}}>
+      <ScrollView>
+      <Text style={{color: 'white', fontSize:30,top:20,textShadowOffset: { width: 10, height: 1 },
+        //textShadowColor: 'blue',
+        //textShadowOpacity: 1,
+        elevation: 10,
+        opacity:1,
+        padding:20,
+        borderRadius:15,
+        // background color must be set
+        backgroundColor : "rgba(150,100,150,0.9)", // invisible color
+        alignSelf:'center'}}>Cartoons</Text>
+        <Text>{'\n\n\n'}</Text>
+        <View style={{width:250,
+          padding:20,
+          alignSelf:'center',
+          backgroundColor:'#eeee',
+          shadowOffset:{width:10,height:10},
+          elevation:10,
+          alignItems:'center'}}>
+          <TouchableHighlight onPress={() => alert('He gives you a thumbs up of good will and acceptance')}>
+            <Image source={require('./bground_img/nice.png')} style={{width:200,height:220}}/>
+          </TouchableHighlight> 
+          <Text>{'\n'}Dude Chad</Text>
+        </View>
+        <Text>{'\n\n\n'}</Text>
+        <View style={{width:250,
+          padding:20,
+          alignSelf:'center',
+          backgroundColor:'#eeee',
+          shadowOffset:{width:10,height:10},
+          elevation:10,
+          alignItems:'center'}}>
+          <TouchableHighlight onPress={() => alert('He wishes you fortune at the end of your long, wearisome path')}>
+            <Image source={require('./bground_img/nice2.png')} style={{width:200,height:220}}/>
+          </TouchableHighlight> 
+          <Text>{'\n'}Brother wishes Luck</Text>
+        </View>
+        <Text>{'\n\n\n'}</Text>
+        <View style={{width:250,
+          padding:20,
+          alignSelf:'center',
+          backgroundColor:'#eeee',
+          shadowOffset:{width:10,height:10},
+          elevation:10,
+          alignItems:'center'}}>
+          <TouchableHighlight onPress={() => alert('He knows what you did last Wednesday')}>
+            <Image source={require('./bground_img/mocker.png')} style={{width:200,height:220}}/>
+          </TouchableHighlight> 
+          <Text>{'\n'}The Mocker</Text>
+        </View>
+        <Text>{'\n\n\n'}</Text>
+        <View style={{width:250,
+          padding:20,
+          alignSelf:'center',
+          backgroundColor:'#eeee',
+          shadowOffset:{width:10,height:10},
+          elevation:10,
+          alignItems:'center'}}>
+          <TouchableHighlight onPress={() => alert('He smiles through tears when you acknowledge his worth')}>
+            <Image source={require('./bground_img/pained.png')} style={{width:200,height:220}}/>
+          </TouchableHighlight> 
+          <Text>{'\n'}At Last</Text>
+        </View>
+        <Text>{'\n\n\n'}</Text>
+        <View style={{width:250,
+          padding:20,
+          alignSelf:'center',
+          backgroundColor:'#eeee',
+          shadowOffset:{width:10,height:10},
+          elevation:10,
+          alignItems:'center'}}>
+          <TouchableHighlight onPress={() => alert("He's done putting up with your sh*t")}>
+            <Image source={require('./bground_img/bacchaman.png')} style={{width:200,height:220}}/>
+          </TouchableHighlight> 
+          <Text>{'\n'}OK</Text>
+        </View>
+        </ScrollView>
+    </ImageBackground>
   </View>
-);
+)};
+//<Image source={require('./bground_img/hehelol.png')} style={{flex: 1, resizeMode: 'cover'}} />
+function getBackground({route})
+{
+  let color='darkblue';
+  if(route.name=='Home')
+  {
+    color = 'darkblue'
+  }
+  else if(route.name=='Feed')
+  {
+    color = 'green'
+  }
+  else if(route.name=='Settings')
+  {
+    color = 'yellow'
+  }
+  return color;
+}
 
 const HomeTabNavigator = ()=> (
   <Tab.Navigator screenOptions={({route})=>({
@@ -223,14 +284,30 @@ const HomeTabNavigator = ()=> (
       {
         iconName = 'cog'
       }
-      return <Icon name={iconName} size = {size} color={color}/>
+      return <Icon name={iconName} size = {23} color={color} />
     }
-  })}>
-    <Tab.Screen name="Home" /*options={{tabBarIcon:({color,size})=>(
-      <Icon name={'add'} color={size} size={size}/>
-    )}}*/ component={HomeStackNavigator}/>
-    <Tab.Screen name="Feed" component={FeedScreen}/>
-    <Tab.Screen name="Settings" component={SettingsScreen}/>
+  })}
+  activeColor= 'orange'
+  inactiveColor= 'white'
+  shifting = {true}
+  /*tabBarOptions={{
+    style: {backgroundColor : 'darkblue',borderTopColor:'darkblue'}
+  }}*/
+  >
+    <Tab.Screen name="Home" options = {{
+      tabBarLabel:'Home',
+      tabBarColor: 'darkblue'
+    }} component={HomeStackScreen}/>
+    <Tab.Screen name="Feed" options = {{
+      tabBarLabel:'Feed',
+      headerTitle:'Feed',
+      tabBarColor: 'rgb(50,50,50)'
+    }}component={FeedScreen}/>
+    <Tab.Screen name="Settings" options = {{
+      tabBarLabel:'Settings',
+      title:'Settings',
+      tabBarColor: 'rgb(94,68,121)'
+    }}component={SettingsScreen}/>
   </Tab.Navigator>
 );
 
@@ -286,7 +363,7 @@ const App= ({navigation}) => {
       </Text>
     </View>*/
     <NavigationContainer >
-      <Stack.Navigator initialRouteName = "Home" 
+      {/* <Stack.Navigator initialRouteName = "Home" 
       screenOptions = {{
         gestureEnabled: true,
         gestureDirection:'vertical',
@@ -294,26 +371,38 @@ const App= ({navigation}) => {
         //cardStyleInterpolator:CardStyleInterpolators.forFadeFromBottomAndroid
         //transitionSpec:{open:config,close:config}
       }}
-      headerMode='float'/*this is for when we want the header to remain fixed */
+      headerMode='float'//this is for when we want the header to remain fixed 
       animation = 'fade' >
         
         <Stack.Screen name = "Home" options={({route})=>({
           title:getHeaderTitle(route),
           headerShown: shouldHeaderBeShown(route)
-        })} /*{{
+        })} {{
           title: 'Home Screen', 
           headerRight: ({navigation})=> (
           <Button
             title = "Alert"
             onPress = {() =>{alert('Hmm')}}
           />)
-        }}*/
-        component = {HomeTabNavigator/*HomeScreen*/} style={{alignText:"center"}}/>
+        }}
+        component = {HomeTabNavigator} style={{alignText:"center"}}/>
         <Stack.Screen name = "Settings" 
         options={{title: 'Settings'}}
         component = {SettingsScreen}/>
-      </Stack.Navigator>
+      </Stack.Navigator> */}
+      <Drawer.Navigator initialRouteName="Home" 
+        drawerStyle = {{backgroundColor:'white'}} 
+      >
+        <Drawer.Screen name="Home" component={HomeTabNavigator/*HomeStackScreen*/}/>
+        <Drawer.Screen name="Details" component={DetailsStackScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
 export default App;
+
+const Styles = StyleSheet.create({
+  bgcolor:{
+    backgroundColor: 'rgb(52, 150, 52)'
+  }
+})
